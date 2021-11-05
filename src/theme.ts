a0f3763ba3a89a7aeeab6f07ce4abf1c83c970b7
podmenato <filipfedin@gmail.com>
2021-11-05 16:26:16 +0100

    Fixing error that prevented compilation

--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,10 @@ import {createTheme} from '@mui/material'
 export const theme = createTheme({
     palette: {
         primary: {
+            main: '#ff0000'
         },
         secondary: {
+            main: '#0249ff'
         },
     },
     typography: {
