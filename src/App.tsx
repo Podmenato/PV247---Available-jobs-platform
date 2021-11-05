bd75d68c60781c5341acdebd64d869860265d9b7
podmenato <filipfedin@gmail.com>
2021-11-05 19:11:54 +0100

    Installing react router dom and adding browser router to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {ThemeProvider} from '@mui/material'
 import {theme} from 'theme'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {EPaths} from 'enums/EPaths'
+import Home from 'components/Home'
 
 function App() {
   return (
       <ThemeProvider theme={theme}>
-        <div className="App">
-
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path={EPaths.HOME} element={<Home/>}
+                />
+            </Routes>
+        </BrowserRouter>
       </ThemeProvider>
   );
 }
