0e34bcfc16f7d11f59db40becf0f9665504c5077
Ludovit <kopcsanyiludovit@gmail.com>
2021-11-07 00:59:10 +0100

    added haaxor eslint config + fixed all errors

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,13 @@
-import {createTheme} from '@mui/material'
+import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#ff0000'
-        },
-        secondary: {
-            main: '#0249ff'
-        },
-    },
-    typography: {
-    },
+	palette: {
+		primary: {
+			main: '#ff0000'
+		},
+		secondary: {
+			main: '#0249ff'
+		}
+	},
+	typography: {}
 });
