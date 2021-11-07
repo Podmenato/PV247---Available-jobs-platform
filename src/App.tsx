6a0c19399ad99f6fcafffd7dacb1f5da7d2e77c1
Ludovit <kopcsanyiludovit@gmail.com>
2021-11-07 11:27:10 +0100

    added offer route

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { theme } from 'theme';
 import { EPaths } from 'enums/EPaths';
 import Home from 'components/Home';
+import Offer from 'components/Offer';
 
 const App = () => (
 	<ThemeProvider theme={theme}>
 		<BrowserRouter>
 			<Routes>
 				<Route path={EPaths.HOME} element={<Home />} />
+				<Route path={EPaths.OFFER} element={<Offer />} />
 			</Routes>
 		</BrowserRouter>
 	</ThemeProvider>
