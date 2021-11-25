1b333c62967c4751290b61f4f88879838a8833cb
Michaela Bajanová <MichaelaBajan@gmail.com>
2021-11-25 23:18:20 +0100

    localization

---
+++ src/localization/sk.ts
@@ -0,0 +1,11 @@
+export default {
+	home: 'Domov',
+	login: 'Prihlásenie',
+	logout: 'Odhlásenie',
+	required: 'Povinné pole',
+	unknown_error: 'Nastala neznáma chyba',
+	email: 'Email',
+	password: 'Heslo',
+	sign_up: 'Registrovať sa',
+	sign_in: 'Prihlásiť sa'
+};
