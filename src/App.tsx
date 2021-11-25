ac2b1ff7dfab9979b97af471eeadb283adc0cc1c
Michaela Bajanová <MichaelaBajan@gmail.com>
2021-11-25 23:18:57 +0100

    added app bar and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { theme } from 'theme';
 import { EPaths } from 'enums/EPaths';
-import Home from 'components/Home';
+import Home from 'pages/Home';
+import Layout from 'components/Layout';
+import { LanguageProvider } from 'hooks/useTranslation';
+import Login from 'pages/Login';
+import { UserProvider } from 'hooks/useUser';
 
 const App = () => (
 	<ThemeProvider theme={theme}>
-		<BrowserRouter>
-			<Routes>
-				<Route path={EPaths.HOME} element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+		<UserProvider>
+			<LanguageProvider>
+				<BrowserRouter>
+					<CssBaseline />
+					<Layout>
+						<Routes>
+							<Route path={EPaths.HOME} element={<Home />} />
+							<Route path={EPaths.LOGIN} element={<Login />} />
+						</Routes>
+					</Layout>
+				</BrowserRouter>
+			</LanguageProvider>
+		</UserProvider>
 	</ThemeProvider>
 );
 
