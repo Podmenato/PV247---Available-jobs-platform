028acd9f245db4d82cae6c6d58ced310d0d72433
Ludovit <kopcsanyiludovit@gmail.com>
2021-11-29 12:19:19 +0100

    filled offer page with offer info

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -9,7 +9,12 @@ import {
 	CardMedia,
 	TextField,
 	FormControl,
-	Input
+	Input,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText
 } from '@mui/material';
 import {
 	Email,
@@ -19,35 +24,54 @@ import {
 	ShareOutlined,
 	Edit,
 	Add,
-	Send
+	Send,
+	AttachMoney,
+	AccessTime,
+	School,
+	PlaylistAddCheck,
+	Timelapse
 } from '@mui/icons-material';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { Link as RouterLink } from 'react-router-dom';
-import { Box } from '@mui/system';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const Offer = () => {
 	const { id } = useParams();
+	const [jobParams, setJobParams] = useState<Record<string, any>>();
+
+	useEffect(() => {
+		axios
+			.request({
+				url: `https://api.apitalks.store/volna-pracovni-mista/${id}`,
+				method: 'GET',
+				headers: {
+					'x-api-key': '27DMzQacy52IFIj5RXYal3MVqtqmTrXb5W17CoO7'
+				}
+			})
+			.then(
+				(response: AxiosResponse) => {
+					console.log(response.data);
+					setJobParams(response.data);
+				},
+				(error: AxiosError) => {
+					setJobParams(undefined);
+				}
+			);
+	}, [id]);
+
 	const [isFavorite, setIsFavorite] = useState<boolean>(false);
 	const [fileName, setFileName] = useState<string>();
-	const isAuthor = true; //right now, everyone is the author:)
 	const handleUpload = (event: ChangeEvent) => {
 		const target = event.target as HTMLInputElement;
 		setFileName(target?.files?.item(0)?.name);
 	};
 
-	return (
+	return jobParams ? (
 		<Grid container spacing={2}>
 			<Grid item xs={12} md={8}>
 				<Card sx={{ padding: '1em' }}>
-					<CardActions sx={{ justifyContent: 'space-between' }}>
-						<Typography variant="h3">Job title</Typography>
+					<CardActions sx={{ justifyContent: 'flex-end' }}>
 						<div>
-							{isAuthor && (
-								<IconButton onClick={() => alert('edit')}>
-									<Edit />
-								</IconButton>
-							)}
 							<IconButton onClick={() => alert('share')}>
 								<ShareOutlined />
 							</IconButton>
@@ -61,16 +85,74 @@ const Offer = () => {
 						</div>
 					</CardActions>
 					<CardContent>
-						Id: <strong>{id}</strong>
-						<br />
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur.Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
-						<FormControl sx={{ width: '100%' }}>
+						<div>
+							<Typography variant="h4">{jobParams?.PROFESE?.nazev}</Typography>
+							<Typography variant="body1">
+								{jobParams?.PROFESE?.doplnek}
+							</Typography>
+						</div>
+						{jobParams?.OBOR?.nazev && (
+							<strong>{jobParams?.OBOR?.nazev}</strong>
+						)}
+						<hr />
+						<List>
+							<ListItem disablePadding>
+								<ListItemIcon>
+									<School />
+								</ListItemIcon>
+								<ListItemText
+									primary={jobParams?.MIN_VZDELANI?.nazev}
+									secondary="Required education"
+								/>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemIcon>
+									<PlaylistAddCheck />
+								</ListItemIcon>
+								<ListItemText
+									primary={Object.entries(jobParams?.VHODNE_PRO)
+										.filter(([_, val]) => val === 'A')
+										.map(([key, _]) => key)}
+									secondary="Suitable for"
+								/>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemIcon>
+									<AccessTime />
+								</ListItemIcon>
+								<ListItemText
+									primary={Object.entries(jobParams?.PRACPRAVNI_VZTAH)
+										.filter(([_, val]) => val === 'A')
+										.map(([key, _]) => key)}
+									secondary="Employment type"
+								/>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemIcon>
+									<Timelapse />
+								</ListItemIcon>
+								<ListItemText
+									primary={jobParams?.SMENNOST?.nazev}
+									secondary="Shifts"
+								/>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemIcon>
+									<AttachMoney />
+								</ListItemIcon>
+								<ListItemText
+									primary={`${jobParams?.MZDA?.min} Kč / ${jobParams?.MZDA?.typMzdy}`}
+									secondary="Minimum salary"
+								/>
+							</ListItem>
+						</List>
+						<hr />
+						{jobParams?.POZNAMKA?.split('\r\n').map(
+							(line: string, id: number) => (
+								<p key={id}>{line}</p>
+							)
+						)}
+						{/*<FormControl sx={{ width: '100%' }}>
 							<TextField
 								label="First name"
 								variant="standard"
@@ -106,13 +188,28 @@ const Offer = () => {
 							<Button variant="contained" type="submit">
 								<Send /> &nbsp;Send inquiry
 							</Button>
-						</FormControl>
+						</FormControl>*/}
 					</CardContent>
 				</Card>
 			</Grid>
 			<Grid item xs={12} md={4}>
 				<Card sx={{ padding: '1em' }}>
-					<Typography>Company details (address w/ map)</Typography>
+					{/* <Typography>Company details</Typography> */}
+					<List>
+						<ListItem disablePadding>
+							<ListItemText
+								primary={jobParams?.FIRMA?.nazev}
+								secondary={`IČO: ${jobParams?.FIRMA?.ic}`}
+							/>
+						</ListItem>
+						{jobParams?.PRACOVISTE?._.split(', ').map(
+							(text: string, id: number) => (
+								<ListItem key={id} disablePadding>
+									<ListItemText primary={text} />
+								</ListItem>
+							)
+						)}
+					</List>
 					<CardMedia>
 						<img
 							src="https://assets-global.website-files.com/6050a76fa6a633d5d54ae714/609147088669907f652110b0_report-an-issue(about-maps).jpeg"
@@ -120,25 +217,68 @@ const Offer = () => {
 							width="100%"
 						/>
 					</CardMedia>
-					<Button
-						variant="contained"
-						href="mailto:#"
-						sx={{
-							width: '100%',
-							margin: '1em 0'
-						}}
-					>
-						<Email /> &nbsp;email
-					</Button>
-					<Button
-						variant="outlined"
-						href="tel:#"
-						sx={{
-							width: '100%'
-						}}
-					>
-						<Phone /> &nbsp;phone
-					</Button>
+					<Typography variant="h5">Contact person</Typography>
+					<List>
+						<ListItem disablePadding>
+							<ListItemText primary={jobParams?.KONOS?._} />
+						</ListItem>
+						<ListItem disablePadding>
+							<ListItemButton
+								component="a"
+								href={`mailto:${jobParams?.KONOS?.email}`}
+							>
+								<ListItemIcon>
+									<Email />
+								</ListItemIcon>
+								<ListItemText primary={jobParams?.KONOS?.email} />
+							</ListItemButton>
+						</ListItem>
+						<ListItem disablePadding>
+							<ListItemButton
+								component="a"
+								href={`tel:${jobParams?.KONOS?.telefon}`}
+							>
+								<ListItemIcon>
+									<Phone />
+								</ListItemIcon>
+								<ListItemText primary={jobParams?.KONOS?.telefon} />
+							</ListItemButton>
+						</ListItem>
+					</List>
+					{jobParams?.KONOS?.email && (
+						<Button
+							variant="contained"
+							href={`mailto:${jobParams?.KONOS?.email}`}
+							sx={{
+								width: '100%',
+								margin: '1em 0'
+							}}
+						>
+							<Email /> &nbsp;email
+						</Button>
+					)}
+					{jobParams?.KONOS?.telefon && (
+						<Button
+							variant="outlined"
+							href={`tel:${jobParams?.KONOS?.telefon}`}
+							sx={{
+								width: '100%'
+							}}
+						>
+							<Phone /> &nbsp;phone
+						</Button>
+					)}
+				</Card>
+			</Grid>
+		</Grid>
+	) : (
+		<Grid container spacing={2}>
+			<Grid item xs={12} md={12}>
+				<Card sx={{ padding: '1em' }}>
+					<Typography variant="h4">
+						Not found. Offer does not exist or the server is not responding,
+						please try again later.
+					</Typography>
 				</Card>
 			</Grid>
 		</Grid>
