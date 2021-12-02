b8037cf17bbff220ef22f55519d2465fea935b4c
podmenato <filipfedin@gmail.com>
2021-12-02 13:56:50 +0100

    Installing icons and adding switch buttons for displaying mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Paper, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
-const Home = () => <div>HOME</div>;
+enum ESearchListDisplay {
+	MAP = 'MAP',
+	LIST = 'List'
+}
+
+const Home = () => {
+	const [displayMode, setDisplayMode] = useState(ESearchListDisplay.MAP);
+
+	const handleAlignment = (
+		_event: React.MouseEvent<HTMLElement>,
+		newDisplayMode: ESearchListDisplay | null
+	) => {
+		if (newDisplayMode !== null) {
+			setDisplayMode(newDisplayMode);
+		}
+	};
+
+	return (
+		<Paper>
+			<Stack direction="row" spacing={4}>
+				<ToggleButtonGroup
+					value={displayMode}
+					exclusive
+					onChange={handleAlignment}
+					aria-label="display mode"
+				>
+					<ToggleButton value={ESearchListDisplay.MAP} aria-label="map">
+						<MapOutlinedIcon />
+					</ToggleButton>
+					<ToggleButton value={ESearchListDisplay.LIST} aria-label="list">
+						<ListOutlinedIcon />
+					</ToggleButton>
+				</ToggleButtonGroup>
+			</Stack>
+		</Paper>
+	);
+};
 
 export default Home;
