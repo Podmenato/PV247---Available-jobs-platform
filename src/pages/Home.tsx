6bb9aedf29ef3eedfd3393cdc90159d2394ff494
podmenato <filipfedin@gmail.com>
2021-12-02 17:28:38 +0100

    Adding function for coordinate search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Paper } from '@mui/material';
-import { LatLng } from 'leaflet';
 
-import JobPinsDisplayMap from 'components/JobPinsDisplayMap';
+import SearchInput from 'components/SearchInput';
 
-enum ESearchListDisplay {
-	MAP = 'MAP',
-	LIST = 'List'
-}
-
-const Home = () => {
-	const [displayMode, setDisplayMode] = useState(ESearchListDisplay.MAP);
-
-	const handleAlignment = (
-		_event: React.MouseEvent<HTMLElement>,
-		newDisplayMode: ESearchListDisplay | null
-	) => {
-		if (newDisplayMode !== null) {
-			setDisplayMode(newDisplayMode);
-		}
-	};
-
-	return (
-		<Paper>
-			<JobPinsDisplayMap markerPosition={new LatLng(49.804, 15.815)} />
-		</Paper>
-	);
-};
+const Home = () => (
+	<Paper>
+		<SearchInput />
+	</Paper>
+);
 
 export default Home;
