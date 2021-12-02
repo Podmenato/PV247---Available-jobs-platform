ee2940526436f1fd81aff31ec97678ac67f74ac9
podmenato <filipfedin@gmail.com>
2021-12-02 17:29:28 +0100

    Starting work on SearchInput.tsx

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+const SearchInput = () => {
+	const [f, setf] = useState();
+	return <Box>Search</Box>;
+};
+
+export default SearchInput;
