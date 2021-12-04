635042ba9df6f87b01bb41d49419dd12b8da657c
podmenato <filipfedin@gmail.com>
2021-12-04 23:59:19 +0100

    Adding data grid MUI

--- src/pages/OffersList.tsx
+++ src/pages/OffersList.tsx
@@ -1,26 +1,49 @@
 import React, { useState } from 'react';
-import { List } from '@mui/material';
 import { useLocation } from 'react-router-dom';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-import Filter from 'components/Filter';
 import useOffers from 'hooks/useOffers';
-import OfferPreview from 'components/OfferPreview';
 import { useFilter } from 'hooks/useFilter';
+import { IJobOffer } from 'interfaces/IJobOffer';
 
 const OffersList = () => {
 	const location = useLocation();
 	const [params] = useState(location.state.params);
 	const offers = useOffers();
 	const filtered = useFilter(offers, params);
+
+	const columns: GridColDef[] = [
+		{ field: 'id', headerName: 'ID', hide: true },
+		{ field: 'education', headerName: 'Education', width: 200 },
+		{
+			field: 'field',
+			headerName: 'Field',
+			width: 150
+		},
+		{
+			field: 'language',
+			headerName: 'Language'
+		}
+	];
+
+	const mapToRows = (offer: IJobOffer) => ({
+		id: offer.uid,
+		education: offer.MIN_VZDELANI.nazev,
+		field: offer.OBOR.nazev,
+		language: offer.JAZYK?.nazev
+	});
+
 	return (
-		<>
-			<Filter />
-			<List>
-				{filtered.map(offer => (
-					<OfferPreview key={offer.uid} jobOffer={offer} />
-				))}
-			</List>
-		</>
+		<div style={{ height: 400, width: '100%' }}>
+			<DataGrid
+				rows={filtered.map((offer: IJobOffer) => mapToRows(offer))}
+				columns={columns}
+				pageSize={5}
+				rowsPerPageOptions={[5]}
+				checkboxSelection
+				disableSelectionOnClick
+			/>
+		</div>
 	);
 };
 
