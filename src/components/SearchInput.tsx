72b57595e6adec974a4626c15f67f0a8d6194e08
Michaela Bajanová <MichaelaBajan@gmail.com>
2021-12-04 18:00:09 +0100

    showing offers and basic filter

--- src/components/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
-
-const SearchInput = () => {
-	const [f, setf] = useState();
-	return <Box>Search</Box>;
-};
-
-export default SearchInput;
